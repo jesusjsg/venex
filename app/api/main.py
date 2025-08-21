d376b6e9dd6cbb225d37586cc3dc04ab054a99cc
jesusjsg <jesusjsgdev@gmail.com>
2025-08-21 16:32:00 -0400

    refactor(api): Restructure API routes

--- app/api/main.py
+++ app/api/main.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from app.api.routes import health
+from app.api.routes import health, help
 
 api_router = APIRouter()
 
 api_router.include_router(health.router, prefix="/v1")
+api_router.include_router(help.router, prefix="/v1")
 
