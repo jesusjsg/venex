28fc9e5133ae201ead6a8866784527a863bb3834
jesusjsg <jesusjsgdev@gmail.com>
2025-08-24 04:57:43 +0000

    refactor: Improve help endpoint

--- app/api/routes/help.py
+++ app/api/routes/help.py
@@ -1,17 +1,19 @@
 from fastapi import APIRouter, Request
 from app.services.supabase_service import SupabaseService
-
+from app.core.settings import settings
 
 router = APIRouter(prefix="/help")
 
 
 @router.get("/")
-async def get_status_server() -> bool:
-    return True
+async def get_status_server() -> dict:
+    return {"status": "ok"}
 
 
-@router.get("/status/db")
-async def get_status_db(request: Request):
-    supabase: SupabaseService = request.app.state.supabase
-    response = await supabase.client.table("currency_rates").select("*").execute()
-    return response
+if settings.ENVIRONMENT == "development":
+
+    @router.get("/status/db")
+    async def get_status_db(request: Request):
+        supabase: SupabaseService = request.app.state.supabase
+        response = await supabase.client.table("currency_rates").select("*").execute()
+        return response
