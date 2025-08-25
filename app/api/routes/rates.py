783bfb49504c4a7a7ed67c994158fa06f81918f2
jesusjsg <jesusjsgdev@gmail.com>
2025-08-25 00:37:40 +0000

    feat(rates): Calculate and return average Binance rate

--- app/api/routes/rates.py
+++ app/api/routes/rates.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
+from starlette.concurrency import run_in_threadpool
+
 from app.services.rates.binance_rate import BinanceRate
+from app.utils import get_average_rate
+
 
 router = APIRouter(prefix="/rates")
 
@@ -7,4 +11,6 @@ router = APIRouter(prefix="/rates")
 @router.get("/binance")
 async def get_binance_rate():
     binance_rate = BinanceRate()
-    return binance_rate.get_binance_rate()
+    rates = await run_in_threadpool(binance_rate.get_binance_rate)
+    avg_rate = await run_in_threadpool(get_average_rate, rates)
+    return {"avg_rate": avg_rate}
