ce1e72699c2c0e7ae51828a6fb9f9d76218e448e
jesusjsg <jesusjsgdev@gmail.com>
2025-08-26 02:47:17 +0000

    refactor(rates): change endpoint to save rates

--- app/api/routes/rates.py
+++ app/api/routes/rates.py
@@ -1,16 +1,22 @@
 from fastapi import APIRouter
 from starlette.concurrency import run_in_threadpool
 
+from app.enum.rate_enum import RateCurrency, RateSource
+from app.services.rate_service import RateService
 from app.services.rates.binance_rate import BinanceRate
-from app.utils import get_average_rate
 
 
 router = APIRouter(prefix="/rates")
 
 
-@router.get("/binance")
-async def get_binance_rate():
+@router.post("/binance")
+async def save_binance_rate():
     binance_rate = BinanceRate()
     rates = await run_in_threadpool(binance_rate.get_binance_rate)
-    avg_rate = await run_in_threadpool(get_average_rate, rates)
-    return {"avg_rate": avg_rate}
+    rate_service = RateService()
+
+    data = await rate_service.save_rates(
+        source=RateSource.BINANCE, currency=RateCurrency.USDT, rates=rates
+    )
+
+    return data
