2f4379694a11e3eb8cd298b4c31e38273a666ef0
jesusjsg <jesusjsgdev@gmail.com>
2025-08-30 03:15:25 +0000

    fix: remove unused rates endpoint

--- app/api/routes/rates.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from starlette.concurrency import run_in_threadpool
-
-from app.enum.rate_enum import RateCurrency, RateSource
-from app.services.rate_service import RateService
-
-
-router = APIRouter(prefix="/binance")
