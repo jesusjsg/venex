1fb92a75c80d0bf0ff7abccaf63ec5a71fc144d6
jesusjsg <jesusjsgdev@gmail.com>
2025-09-02 02:18:14 +0000

    fix: Use USDT instead of USD for Binance job

--- app/jobs/binance_job.py
+++ app/jobs/binance_job.py
@@ -6,7 +6,7 @@ from app.services.rates.binance_rate import BinanceRate
 from app.services.rate_service import RateService
 from app.enum.rate_enum import RateCurrency, RateSource
 
-currency = RateCurrency.USD
+currency = RateCurrency.USDT
 url = settings.BINANCE_RATES_USDT_URL
 headers = {"Content-Type": "application/json"}
 payload = {
