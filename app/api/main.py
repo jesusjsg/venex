97eae053de1e034aa38d82a3d62f32be1fb1eae7
jesusjsg <jesusjsgdev@gmail.com>
2025-09-08 02:56:24 +0000

    feat: Add BCV API endpoint

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from app.api.routes import help
-from app.api.routes import binance
+from app.api.routes import bcv, help, binance
 
 api_router = APIRouter()
 
 api_router.include_router(help.router, tags=["Help"])
 api_router.include_router(binance.router, tags=["Binance"])
+api_router.include_router(bcv.router, tags=["BCV"])
